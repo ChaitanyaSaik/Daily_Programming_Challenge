7af6ae09e657241291aaa0647b08013832e25028
ChaitanyaSaik <159690333+ChaitanyaSaik@users.noreply.github.com>
2024-09-10 09:28:03 +0530

    Add files via upload

---
+++ Q1.py
@@ -0,0 +1,47 @@
+def sort012(arr):
+    low = 0
+    mid = 0
+    high = len(arr) - 1
+
+    while mid <= high:
+        if arr[mid] == 0:
+            arr[low], arr[mid] = arr[mid], arr[low]
+            low += 1
+            mid += 1
+        elif arr[mid] == 1:
+            mid += 1
+        elif arr[mid] == 2:
+            arr[high], arr[mid] = arr[mid], arr[high]
+            high -= 1
+        else:
+            print()
+
+
+arr = [0, 1, 2, 1, 0, 2, 1, 0]
+sort012(arr)
+print(arr) 
+# Test case 2
+arr1 = [2, 0, 1, 2, 0, 1, 0, 2, 1]
+sort012(arr1)
+print(arr1) 
+
+# Test case 3
+arr2 = [1, 2, 0, 1, 2, 0, 1, 2, 0]
+sort012(arr2)
+print(arr2) 
+
+# Test case 4
+arr3 = [1, 0, 2]
+sort012(arr3)
+print(arr3) 
+
+# Test case 5
+arr4 = [2, 2, 2, 2, 2]
+sort012(arr4)
+print(arr4) 
+
+# Test case 6
+arr5 = [0, 0, 0, 0]
+sort012(arr5)
+print(arr5) 
+
