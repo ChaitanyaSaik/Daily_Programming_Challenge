2bd601691c7a98a2b67a1399da8fbf4e8f9a96e9
ChaitanyaSaik <159690333+ChaitanyaSaik@users.noreply.github.com>
2024-09-16 15:33:31 +0530

    Add files via upload

---
+++ Q8.py
@@ -0,0 +1,8 @@
+def Q8(arr):
+    list_values=arr.split(" ")
+    list_values=list_values[::-1]
+    reversed_values=" ".join(list_values)
+    print(reversed_values)
+
+arr="the sky is blue"
+Q8(arr)
